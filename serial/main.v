7505cb329474267458c88d4d023fe0a563fecc0c
FreeLance <freelance@freelance-VirtualBox.(none)>
2014-07-06 01:54:31 +1000

    added camera

--- serial/main.v
+++ serial/main.v
@@ -1,10 +1,15 @@
-module main(CLK,HSYNC,VSYNC,R,G,B);
+module main(CLK,HSYNC,VSYNC,R,G,B,XCLK,PCLK,D,HIN,VIN);
 	input CLK;
 	output reg HSYNC;
 	output reg VSYNC;
 	output reg R;
 	output reg G;
 	output reg B;
+	output reg XCLK;
+	input PCLK;
+	input [7:0] D;
+	input HIN;
+	input VIN;
 	
 	//hsync = 1589
 	//pulse = 191
@@ -15,33 +20,68 @@ module main(CLK,HSYNC,VSYNC,R,G,B);
 	reg [15:0] VCNT;
 	reg drawH;
 	reg drawV;
+	reg [7:0] CAMDAT [1:0];
+	reg BCNT;
+	reg [15:0] PXCNT;
+	reg [15:0] PVCNT;
+	reg [255:0] RDAT [127:0];
+	reg [255:0] GDAT [127:0];
+	reg [255:0] BDAT [127:0];
+	reg CR;
+	reg CG;
+	reg CB;
+	always @(posedge PCLK)begin
+		if(VIN)
+			PVCNT <= 7'd0;
+		else
+			if(!HIN)begin
+				if(PXCNT != 8'h0)
+					PVCNT <= PVCNT + 7'd1;
+				BCNT <= 1'b0;
+				PXCNT <= 8'h0;
+			end else begin
+				BCNT <= !BCNT;
+				if(BCNT)
+					PXCNT <= PXCNT + 8'd1;
+			end
+		CAMDAT[BCNT] <= D;
+	end
 	
 	always @(negedge CLK) begin
+		XCLK <= ~XCLK;
 		HCNT <= HCNT + 16'h1;
 		if(HCNT == 16'd1588)begin
 			HCNT <= 16'h0;
+			drawH <= 1'b1;
+		end else if(HCNT == 16'd1302)begin
 			HSYNC <= 1'b0;
 			VCNT <= VCNT + 16'h1;
-		end else if(HCNT == 16'd188)begin
+		end else if(HCNT == 16'd1493)begin
 			HSYNC <= 1'b1;
-		end else if(HCNT == 16'd283)begin
-			drawH <= 1'b1;
-		end else if(HCNT == 16'd1541)begin
+		end else if(HCNT == 16'd1270)begin
 			drawH <= 1'b0;
 		end
-		if(VCNT == 16'd524)begin
-			VCNT <= 16'h0;
+		if(VCNT == 16'd513)begin
 			VSYNC <= 1'b0;
-		end else if(VCNT == 16'd1)begin
+		end else if(VCNT == 16'd515)begin
 			VSYNC <= 1'b1;
-		end else if(VCNT == 16'd11)begin
+		end else if(VCNT == 16'd524)begin
 			drawV <= 1'b1;
-		end else if(VCNT == 16'd491)begin
+			VCNT <= 16'h0;
+		end else if(VCNT == 16'd480)begin
 			drawV <= 1'b0;
 		end
-		R <= drawH && drawV && HCNT[4];
-		G <= drawH && drawV && VCNT[2];
-		B <= drawH && drawV && HCNT[2];
+		if(!BCNT && HIN && !VIN && PVCNT[15:8]==0 && PXCNT[15:9]==0) begin		
+			RDAT[PVCNT[7:1]][PXCNT[8:1]] <= CAMDAT[1][6];
+			GDAT[PVCNT[7:1]][PXCNT[8:1]] <= CAMDAT[1][7];
+			BDAT[PVCNT[7:1]][PXCNT[8:1]] <= CAMDAT[1][5];
+		end
+		CR <= RDAT[VCNT[7:1]][HCNT[9:2]];
+		CG <= GDAT[VCNT[7:1]][HCNT[9:2]];
+		CB <= BDAT[VCNT[7:1]][HCNT[9:2]];
+		R <= drawH && drawV && CR;
+		G <= drawH && drawV && CG;
+		B <= drawH && drawV && CB;
 		
 	end 
 	
